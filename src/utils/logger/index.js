14d95fa283fc723a3c633f95ae0643dcd12124e8
ruslanglaznyov <ruslan.glaznyov@gmail.com>
2020-01-18 13:17:47 +0200

    add warn message to logger prototype

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -3,4 +3,6 @@ module.exports = {
     error: e =>  console.error(e),
     // eslint-disable-next-line no-undef
     info: msg => console.log(msg),
+    // eslint-disable-next-line no-undef
+    warn: msg => console.log(msg)
 };
