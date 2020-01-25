9172bc42d9551011fdcb4a84a8afa06701a23d1b
Yehor Koniukhov <koniukhov.yehor@gmail.com>
2020-01-25 21:59:08 +0200

    connection to database from config

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -3,11 +3,8 @@ const fs = require('fs')
 const logger = require('../utils/logger')
 const config = require('./../config/env')
 
-const conString = 'postgres://lzxdfpik:bILzLD1yrycRbYZb74fckcKt2tJTiqZF@balarama.db.elephantsql.com:5432/lzxdfpik'
-
 const init = async () => {
-  // TODO: read environment variables (need to add after merge)
-  const client = new pg.Client(conString)
+  const client = new pg.Client(config.db)
   try {
     await client.connect()
     const sql = await fs.readFileSync(config.db.initDBfILE, { encoding: 'UTF-8' })
